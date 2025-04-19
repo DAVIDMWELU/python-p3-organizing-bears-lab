3b1d9bf74f4bc93f4d5088d093203e815988b7f7
DAVIDMWELU <davidmwelu@gmail.com>
2025-04-20 01:52:13 +0300

    organizing bears

---
+++ lib/insert.sql
@@ -0,0 +1,12 @@
+-- insert.sql
+
+-- Insert 8 bears into the bears table
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES
+('Mr. Chocolate', 20, 'M', 'Brown', 'Aggressive', 1),
+('Rowdy', 10, 'M', 'Black', 'Friendly', 1),
+('Tabitha', 6, 'F', 'White', 'Shy', 1),
+('Sergeant Brown', 15, 'M', 'Brown', 'Aggressive', 1),
+('Melissa', 13, 'F', 'Black', 'Calm', 1),
+('Grinch', 2, 'M', 'Green', 'Playful', 1),
+('Wendy', 6, 'F', 'Brown', 'Friendly', 1),
+('Unnamed', 5, 'F', 'Gray', 'Curious', 1);
